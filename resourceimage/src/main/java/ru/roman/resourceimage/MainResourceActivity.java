e40be6fb154b585aaeb6e1643493972631dc2f59
Roman Malkov <roman-li-83@yandex.ru>
2017-12-20 22:58:04 +0500

    Add module ResourceImage

---
+++ resourceimage/src/main/java/ru/roman/resourceimage/MainResourceActivity.java
@@ -0,0 +1,13 @@
+package ru.roman.resourceimage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainResourceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_resource);
+    }
+}
